4858900faac03ef54fa92e93ea347d4810c3fbdb
Stas <staszhingel@icloud.com>
2021-08-12 11:22:50 +0300

    Initial Commit

---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Стас Жингель on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
